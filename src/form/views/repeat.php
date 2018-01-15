e786c07bacbdecd92d9015ebd392753086512fbd
Marco Milon <marco.milon@avantica.net>
2018-01-15 18:02:05 -0500

    Add index to array when using repeate block

--- src/form/views/repeat.php
+++ src/form/views/repeat.php
@@ -10,6 +10,19 @@
 document.getElementById("new-item__button-<?= $uniqueId ?>").addEventListener("click", function() {    
     var itm = document.getElementById("repeater-<?= $uniqueId ?>").lastElementChild;
     var cln = itm.cloneNode(true);
+    
+    var inputNodes = cln.getElementsByTagName('input');
+    for(var i = 0; i < inputNodes.length; i++){
+        var inputNode = inputNodes[i];
+        var index = inputNode.name.match(/[0-9]+/);
+        var tokens = inputNode.name.split(/[0-9]+/);
+        
+        var newIndex = parseInt(index) + 1;
+        var name = tokens[0] + newIndex + tokens[1];
+        inputNode.name = name;
+        inputNode.value = '';
+    }
+    
     document.getElementById("repeater-<?= $uniqueId ?>").appendChild(cln);
 });
 </script>
--- tests/unit/micro/form/Builder.php
+++ tests/unit/micro/form/Builder.php
@@ -96,7 +96,7 @@ class Builder extends atoum
     
         $builder = new \micro\form\Builder();
         $form = $builder->render($microForm);
-        $this->string($form)->matches('#new-item__button-[a-z0-9]{13}#')->matches('#repeater-[a-z0-9]{13}#')->contains('repeater')->contains('script')->contains('users')->contains('users[]');
+        $this->string($form)->matches('#new-item__button-[a-z0-9]{13}#')->matches('#repeater-[a-z0-9]{13}#')->contains('repeater')->contains('script')->contains('users')->contains('users[0]');
     }
     
 }
